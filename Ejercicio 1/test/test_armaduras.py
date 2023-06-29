aefca0a15e5d7062c9eff812d4fe0111b3a1e389
crltsnch <crltsnch@myuax.com>
2023-06-29 20:36:35 +0200

    test

---
+++ Ejercicio 1/test/test_armaduras.py
@@ -0,0 +1,6 @@
+import unittest
+from src.armaduras import ArmadurasController
+
+class TestArmadurasController(unittest.TestCase):
+    def test_agregar_armadura(self):
+        controller = ArmadurasController()
