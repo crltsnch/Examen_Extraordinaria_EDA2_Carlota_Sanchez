7bc53d2cf30a3e8649384874641e9bf446a35ae0
crltsnch <crltsnch@myuax.com>
2023-06-29 20:38:24 +0200

    funcion test agregar

--- Ejercicio 1/test/test_armaduras.py
+++ Ejercicio 1/test/test_armaduras.py
@@ -4,3 +4,10 @@ from src.armaduras import ArmadurasController
 class TestArmadurasController(unittest.TestCase):
     def test_agregar_armadura(self):
         controller = ArmadurasController()
+        controller.agregar_armadura("MK-8888", "Mark I")
+        controller.assertEqual(len(controller.armaduras), 1)
+    
+    def test_obtener_armadura_existente(self):
+        controller = ArmadurasController()
+        controller.agregar_armadura("MK-8888", "Mark I")
+        
