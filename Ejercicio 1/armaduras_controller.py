d9bc624b946ccc47e67e9b8d9e5cf2bc18a97585
crltsnch <crltsnch@myuax.com>
2023-06-29 19:54:35 +0200

    funcion agregar armadura

--- Ejercicio 1/armaduras_controller.py
+++ Ejercicio 1/armaduras_controller.py
@@ -4,3 +4,8 @@ class ArmadurasController:
     def __init__(self):
         self.armaduras = {}
   
+    def agregar_armadura(self, nombre, rango):
+        armadura = Armadura(nombre, rango)
+        self.armaduras[nombre] = armadura
+    
+    
