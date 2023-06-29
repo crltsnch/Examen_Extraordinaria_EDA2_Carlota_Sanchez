0e17bbff06b619ae0a8b2e3fd693b137eaea4aca
crltsnch <crltsnch@myuax.com>
2023-06-29 20:23:49 +0200

    1.2

---
+++ Ejercicio 1/1.2.py
@@ -0,0 +1,17 @@
+from .armaduras import Armadura
+
+def main():
+    armaduras = []
+
+    # Agregar armaduras a la lista
+    armaduras.append(Armadura("MK-8888", "Mark I"))
+    armaduras.append(Armadura("MK-8889", "Mark V")) 
+    armaduras.append(Armadura("MK-8890", "Mark XLII"))
+
+    # Ejecutar el método calificación de cada objeto
+    for armadura in armaduras:
+        calificacion = armadura.calificacion()
+        print(f"Calificación de {armadura.nombre}: {calificacion}")
+
+if __name__ == "__main__":
+    main()
