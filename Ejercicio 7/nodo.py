ee891a1f61e15f6dc10050e7041c29409a7002b1
crltsnch <crltsnch@myuax.com>
2023-06-30 10:13:10 +0200

    .

--- Ejercicio 7/nodo.py
+++ Ejercicio 7/nodo.py
@@ -1,3 +1,7 @@
 class Node:
     def __init__(self, symbol, freuqency):
-        
+        self.symbol = symbol
+        self.frequency = freuqency
+        self.left = None
+        self.right = None
+    
