db898fb6d128423ee2b73cdcc75cb7ca4164ed27
crltsnch <crltsnch@myuax.com>
2023-06-30 11:17:36 +0200

    .

--- Ejercicio 7/test/test_arbol_huffman.py
+++ Ejercicio 7/test/test_arbol_huffman.py
@@ -1,13 +1,21 @@
-from arbol_huffman import arbol_huffman
+from ..src import arbol_huffman
 
 def test_compress():
     freq = {'A':0.2, 'F':0.17, '1':0.13, '3':0.21, '0':0.05, 'M':0.09, 'T':0.15}
     message = "AF10MTF3130A0M"
-    expected_ouput = "1100000110100011000100101011001110011100101101000111000101"
-    assert arbol_huffman.compress(message, freq) == expected_ouput
+    expected_ouput = "0011110010101011110111011000110100010101011"
+    result = arbol_huffman.compress(message, freq)
+    assert result == expected_ouput
+    print("Test de compresión exitoso")
 
 def test_decompress():
     freq = {'A':0.2, 'F':0.17, '1':0.13, '3':0.21, '0':0.05, 'M':0.09, 'T':0.15}
-    encoded_message = "1100000110100011000100101011001110011100101101000111000101"
+    encoded_message = "0011110010101011110111011000110100010101011"
     expected_ouput = "AF10MTF3130A0M"
-    assert arbol_huffman.decompress(encoded_message, freq) == expected_ouput
+    result = arbol_huffman.decompress(encoded_message, freq)
+    assert result == expected_ouput
+    print("Test de descompresión exitoso")
+
+if __name__ == "__main__":
+    test_compress()
+    test_decompress()
