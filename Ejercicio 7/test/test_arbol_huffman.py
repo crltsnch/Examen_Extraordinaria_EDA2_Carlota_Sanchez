fda18102f8c61503ab9949e49ac9816ec24065ce
crltsnch <crltsnch@myuax.com>
2023-07-01 11:40:17 +0200

    guardar

--- Ejercicio 7/test/test_arbol_huffman.py
+++ Ejercicio 7/test/test_arbol_huffman.py
@@ -1,4 +1,4 @@
-from ..src import arbol_huffman
+from src.arbol_huffman import arbol_huffman
 
 def test_compress():
     freq = {'A':0.2, 'F':0.17, '1':0.13, '3':0.21, '0':0.05, 'M':0.09, 'T':0.15}
