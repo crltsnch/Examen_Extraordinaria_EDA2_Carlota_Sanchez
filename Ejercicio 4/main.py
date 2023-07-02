c2a6c527efa4c81d2fd264214e93bc22e022fdec
crltsnch <crltsnch@myuax.com>
2023-07-02 19:33:34 +0200

    ejercicio 4

--- Ejercicio 4/main.py
+++ Ejercicio 4/main.py
@@ -4,4 +4,18 @@ def main():
     mochila = Pila()
     mochila.apilar("libro")
     mochila.apilar("anillo de poder")
-    mochila.apilar("")
+    mochila.apilar("linterna")
+    mochila.apilar("llaves")
+    mochila.apilar("piedra preciosa")
+
+    encontrado, objetos_sacados = hijo_sin_amor(mochila)
+
+    if encontrado:
+        print("Se encontró un anillo de poder!")
+    else:
+        print("No se encontró un anillo de poder")
+
+    print(f"Objetos sacados: {objetos_sacados}")
+
+if __name__ == "__main__":
+    main()
