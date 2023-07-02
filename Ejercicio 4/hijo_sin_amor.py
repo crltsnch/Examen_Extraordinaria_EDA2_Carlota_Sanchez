6511d03017126692ff8d7ada10a10b2e5372b7f2
crltsnch <crltsnch@myuax.com>
2023-07-02 19:27:54 +0200

    fucnion main

--- Ejercicio 4/hijo_sin_amor.py
+++ Ejercicio 4/hijo_sin_amor.py
@@ -5,3 +5,9 @@ def hijo_sin_amor(mochila, objetos_sacados=0):
         return False, objetos_sacados
     
     objeto = mochila.desapilar()
+    objetos_sacados += 1
+
+    if objeto == "anillo de poder":
+        return True, objetos_sacados
+    
+    return hijo_sin_amor(mochila, objetos_sacados)
---
+++ Ejercicio 4/main.py
@@ -0,0 +1,7 @@
+from hijo_sin_amor import *
+
+def main():
+    mochila = Pila()
+    mochila.apilar("libro")
+    mochila.apilar("anillo de poder")
+    mochila.apilar("")
