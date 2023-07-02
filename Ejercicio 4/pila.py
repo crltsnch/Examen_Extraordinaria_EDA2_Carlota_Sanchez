323262ae926308ebc62126aa767727ca3b58c6c5
crltsnch <crltsnch@myuax.com>
2023-07-02 19:19:14 +0200

    ejercicio 4, clase pila

---
+++ Ejercicio 4/pila.py
@@ -0,0 +1,15 @@
+class Pila:
+    def __init__(self):
+        self.items = []
+    
+    def esta_vacia(self):
+        return len(self.items) == 0
+    
+    def apilar(self, item):
+        self.items.append(item)
+    
+    def desapilar(self):
+        if self.esta_vacia():
+           return None
+        return self.items.pop()
+
