38ccd647021ef1ca78ec3f7cf8f9bce8319c1465
crltsnch <crltsnch@myuax.com>
2023-07-02 13:15:22 +0200

    errores

---
+++ Ejercicio 3/src/main.py
@@ -0,0 +1,13 @@
+from artefactosvaliosos import ArtefactoValioso
+
+def main():
+    a1 = ArtefactoValioso(20, "Conserva de frutas", 500, "2023-12-31")
+    a2 = ArtefactoValioso(10, "Joyas antiguas", 1000, "2024-06-30")
+    a3 = ArtefactoValioso(15, "Pintura famosa", 1500, "2023-08-15")
+
+    print(a1)
+    print(a2)
+    print(a3)
+
+if __name__ == "__main__":
+    main()
--- Ejercicio 3/test_artefactosvaliosos.py
+++ Ejercicio 3/test_artefactosvaliosos.py
@@ -4,7 +4,18 @@ from src.artefactosvaliosos import ArtefactoValioso
 class TestArtefactosValiosos(unittest.TestCase):
     def test_mostrar_datos_ordenados(self):
         artefactos = []
-        artefactos.append(ArtefactoValioso(20, "Conserva de frutas", 500, "31-12-2023"))
-        artefactos.append(ArtefactoValioso(10, "Joyas antiguas", 1000, "30-06-2024"))
-        artefactos.append(ArtefactoValioso(15, "Pintura famosa", 1500, "15-08-2023"))
+        artefactos.append(ArtefactoValioso(20, "Conserva de frutas", 500, "2023-12-31"))
+        artefactos.append(ArtefactoValioso(10, "Joyas antiguas", 1000, "2024-06-30"))
+        artefactos.append(ArtefactoValioso(15, "Pintura famosa", 1500, "2023-08-15"))
         
+        artefactos_ordenados = sorted(artefactos, key=lambda artefacto: artefacto.fecha_caducidad)
+
+        for artefacto in artefactos:
+            if artefacto.nombre == "Conserva de frutas":
+                artefacto.precio = 600
+        
+        for artefacto in artefactos_ordenados:
+            print(artefacto)
+
+if __name__ == "__main__":
+    unittest.main()
