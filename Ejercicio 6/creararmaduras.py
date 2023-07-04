98a3708011a17e336aef280ddff1a080508d489a
crltsnch <crltsnch@myuax.com>
2023-07-04 20:15:24 +0200

    funcion crear tablas hash

--- Ejercicio 6/creararmaduras.py
+++ Ejercicio 6/creararmaduras.py
@@ -7,19 +7,27 @@ def generar_armaduras():
     return f'{legion}-{digitos}'
 
 #Generar 2000 armaduras
-armaduras = []
-for i in range(2000):
-    armadura = generar_armaduras()
-    armaduras.append(armadura)
+def generar_armaduras():
+    armaduras = []
+    for i in range(2000):
+        armadura = generar_armaduras()
+        armaduras.append(armadura)
+    return armaduras
+
 
 #Crear las tablas hash encadenadas
-tabla_codigo = {}
-tabla_legion = {}
+def crear_tablas_hash(armaduras):
+    tabla_codigo = {}
+    tabla_legion = {}
 
-for armadura in armaduras:
-    codigo_hash = armadura[-3:]
-    legion_hash = armadura[:2]
+    for armadura in armaduras:
+        codigo_hash = armadura[-3:]
+        legion_hash = armadura[:2]
 
-    if codigo_hash in tabla_codigo:
-        tabla_codigo[codigo_hash] = []
-    tabla_codigo[codigo_hash].append(armadura)
+        if codigo_hash not in tabla_codigo:
+            tabla_codigo[codigo_hash] = []
+        tabla_codigo[codigo_hash].append(armadura)
+
+        if legion_hash not in tabla_legion:
+            tabla_legion[legion_hash] = []
+        tabla_legion[legion_hash].append(armadura)
