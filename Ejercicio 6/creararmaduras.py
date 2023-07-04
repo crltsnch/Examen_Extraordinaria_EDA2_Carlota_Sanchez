ac3e6246eba9b7997c42f8b2d387943bed081f4e
crltsnch <crltsnch@myuax.com>
2023-07-04 20:44:40 +0200

    funcion eleminar

--- Ejercicio 6/creararmaduras.py
+++ Ejercicio 6/creararmaduras.py
@@ -1,7 +1,7 @@
 import random
 
 #Función para generar un código de armadura
-def generar_armaduras():
+def generar_codigo():
     legion = random.choice(['FL', 'TF', 'TK', 'CT', 'FN', 'FO'])
     digitos = random.randint(1000, 9999)
     return f'{legion}-{digitos}'
@@ -31,3 +31,19 @@ def crear_tablas_hash(armaduras):
         if legion_hash not in tabla_legion:
             tabla_legion[legion_hash] = []
         tabla_legion[legion_hash].append(armadura)
+
+    return tabla_codigo, tabla_legion
+
+def eliminar_armadura_desertor(tabla_codigo, tabla_legion):
+    if 'FN-2187' in tabla_legion.get('FN', []):
+        tabla_legion['FN'].remove('FN-2187')
+    
+    codigo_hash = '187'
+    if codigo_hash in tabla_codigo and 'FN-2187' in tabla_codigo[codigo_hash]:
+        tabla_codigo[codigo_hash].remove('FN-2187')
+
+def obtener_armaduras_asalto(tabla_codigo):
+    armaduras_asalto = []
+    for armaduras in tabla_codigo.values():
+        for armadura in armaduras:
+    
