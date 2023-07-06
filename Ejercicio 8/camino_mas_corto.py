6d777f7672a696d6d02c613b10267bdc4815d5df
crltsnch <crltsnch@myuax.com>
2023-07-06 10:57:55 +0200

    funcion heap vacio y ambiar prioridad

--- Ejercicio 8/camino_mas_corto.py
+++ Ejercicio 8/camino_mas_corto.py
@@ -1,5 +1,7 @@
 from nodos import NodoArista, NodoVertice, Grafo, Arista
 from camino_mas_corto import tamanio
+from heap import Heap, arribo_h, atencion_h, heap_vacio, buscar_h
+from pila import Pila, apilar, desapilar
 
 
 def camino_mas_corto(grafo, origen, destino):
@@ -14,3 +16,12 @@ def camino_mas_corto(grafo, origen, destino):
             arribo_h(no_visitados, [aux, None], inf)
         aux = aux.sig
 
+    while not heap_vacio(no_visitados):
+        dato = atencion_h(no_visitados)
+        apilar(camino, dato)
+        aux = dato[1][0].adyacentes.inicio
+        while aux is not None:
+            pos = buscar_h(no_visitados, aux.destino)
+            if no_visitados.vector[pos][0] > dato[0] + aux.info:
+                no_visitados.vector[pos][1][1] = dato[1][0].info
+                cambiar_prioridad(no_visitados, pos, dato[0] + aux.info)
