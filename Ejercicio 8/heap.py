34fcb75a06b43f4817ab854d58fb39bb102e2afc
crltsnch <crltsnch@myuax.com>
2023-07-06 10:47:07 +0200

    heap

--- Ejercicio 8/heap.py
+++ Ejercicio 8/heap.py
@@ -18,3 +18,27 @@ class Heap:
     def atencion_h(self):
         if len(self.heap) == 0:
             raise IndexError("El heap está vacío")
+        
+        elemento = self.heap[0]
+        self.heap[0] = self.heap[-1]
+        self.heap.pop()
+        self._sift_down(0)
+        return elemento
+    
+    def _sift_down(self, index):
+        size = len(self.heap)
+        while index < size:
+            child = 2 * index + 1
+            if child >= size:
+                break
+
+            if child + 1 < size and self.heap[child+1] < self.heap[child]:
+                child += 1
+            
+            if self.heap[child] < self.heap[index]:
+                self.heap[child], self.heap[index] = self.heap[index], self.heap[child]
+                index = child
+            
+            else:
+                break
+            
